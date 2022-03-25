e2dc0c36a2c27601cadfa97ce65fc43feb95dcb0
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2022-03-25 21:23:49 +1000

    fix: delete unused files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,2 @@
-export * from './useAllUsers'
 export * from './useCache'
 export * from './useCRUD'
--- src/schema/model/types.ts
+++
@@ -1,7 +0,0 @@
-import { NexusGenFieldTypes } from 'schema/generated/nexusTypes'
-
-export type ProjectSchema = NexusGenFieldTypes['Project']
-
-export type TaskSchema = NexusGenFieldTypes['Task']
-
-export type UserSchema = NexusGenFieldTypes['User']
